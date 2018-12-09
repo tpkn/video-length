b6d633bb0249075f48ee0c6ca6d93a81911409d6
tpkn <tpkn@users.noreply.github.com>
2018-12-09 21:10:19 +0300

    Add files via upload

--- video-length.js
+++ video-length.js
@@ -2,25 +2,23 @@
  * Video Length, http://tpkn.me/
  */
 
-let util = require('util');
-let exec = util.promisify(require('child_process').exec);
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
 
 async function videoLength(file, options = {}){
-   let bin = 'ffprobe';
    let len = 0;
-
-   if(typeof options.bin === 'string'){
-      bin = options.bin;
-   }
+   let { bin = 'ffprobe', debug = false } = options;
 
    try {
+
       let data = await exec(`${bin} -i "${file}" -show_entries format=duration -v quiet -of csv="p=0"`);
       if(typeof data.stdout !== 'undefined'){
          data = data.stdout;
       }
       len = parseFloat(data);
+
    }catch(err){
-      if(typeof options.debug !== 'undefined' && options.debug){
+      if(debug){
          console.log(err);
       }
    }
